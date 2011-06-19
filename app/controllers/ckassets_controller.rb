f9adb51ebd552697dc27728329376b00422fbb2f
hazg <hazg@mail.ru>
2011-06-19 18:18:55 +0400

    ups

--- app/controllers/ckassets_controller.rb
+++
@@ -1,9 +0,0 @@
-class CkassetsController < ApplicationController
-	no_login_required
-	radiant_layout 'Plain' 
-  
-  def get
-    asset = Asset.find(params[:id])
-    redirect_to asset.thumbnail(params[:size])
-  end
-end
--- config/routes.rb
+++
@@ -1,3 +0,0 @@
-ActionController::Routing::Routes.draw do |map|
-  map.match 'ckassets/:id/:size', :controller => 'ckassets', :action => 'get'
-end
